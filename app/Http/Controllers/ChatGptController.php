7916a57e79fd353aaa0fd64e309bc19169b5905b
Michael Klopf <klopf.michael@gmail.com>
2023-03-05 20:40:23 +0100

    initial commit with working chatgpt example.

---
+++ app/Http/Controllers/ChatGptController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ChatGptController extends Controller
+{
+    public function index()
+    {
+        return view('chatgpt.index');
+    }
+
+    public function ask(Request $request)
+    {
+        $prompt = $request->input('prompt');
+        $response = $this->askToChatGPT($prompt);
+
+        return view('chatgpt.response', ['response' => $response]);
+    }
+
+    private function askToChatGPT($prompt)
+    {
+        $response = Http::withoutVerifying()
+            ->withHeaders([
+                'Authorization' => 'Bearer ' . env('CHATGPT_API_KEY'),
+                'Content-Type' => 'application/json',
+            ])->post('https://api.openai.com/v1/engines/text-davinci-003/completions', [
+                "prompt" => $prompt,
+                "max_tokens" => 1000,
+                "temperature" => 0.5
+            ]);
+        
+        if (! $response->successful()) {
+            dd($response);
+        }
+
+        return $response->json()['choices'][0]['text'];
+    }
+}
