43735e5291f78417869215586ae65dd3b0931054
Michael Klopf <klopf.michael@gmail.com>
2023-03-07 12:19:26 +0100

    Style view.

--- resources/js/Pages/Chat/Index.vue
+++ resources/js/Pages/Chat/Index.vue
@@ -1,27 +1,41 @@
 <template>
-    <div>OpenAI Hustling: Ask something to ChatGPT</div>
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h1 class="mt-4">Text completions API: Ask something to ChatGPT</h1>
 
-    <div>
-        <form @submit.prevent="submit">
-            <div>
-                <input
-                    type="text"
-                    class=""
-                    name="prompt"
-                    placeholder="Ask something..."
-                    v-model="form.prompt"
-                >
-            </div>
+        <div class="mt-8">
+            <form @submit.prevent="submit">
+                <div class="relative">
+                    <label
+                        for="prompt"
+                        class="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+                    >Prompt</label>
+                    <div class="mt-2">
+                        <input
+                            type="text"
+                            name="prompt"
+                            id="prompt"
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3"
+                            placeholder="Ask something..."
+                            v-model="form.prompt"
+                        />
+                    </div>
+                </div>
 
-            <div>
-                Answer is {{ answer }}
-            </div>
+                <div class="sm:col-span-2 mt-8 ml-1">
+                    <dt class="text-sm font-medium text-gray-500">Answer</dt>
+                    <dd
+                        class="mt-1 max-w-prose space-y-5 text-sm text-gray-900"
+                        v-html="answer"
+                    />
+                </div>
 
-            <button
-                type="submit"
-                class=""
-            >Send</button>
-        </form>
+                <button
+                    type="submit"
+                    :disabled="isAnswering"
+                    class="mt-4 rounded-md bg-indigo-600 py-1.5 px-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50"
+                >Send prompt</button>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -33,15 +47,22 @@ const form = ref({
     prompt: '',
 });
 
-const answer = ref('');
+const answer = ref('...');
+
+const isAnswering = ref(false);
 
 const submit = () => {
+    isAnswering.value = true;
+
     axios
         .post('/chatgpt/ask', form.value)
         .then((response) => {
+            isAnswering.value = false;
             answer.value = response.data.choices[0].text;
         })
         .catch((error) => {
+            isAnswering.value = false;
+            answer.value = "Something went wrong, it couldn't find an answer."
             console.log('whoopsy', error);
         });
 };
